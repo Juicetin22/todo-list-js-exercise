7a58eb8694407615f20e9a3dec9425b8c7c35803
Justin Huang <justin.2260@gmail.com>
2021-12-03 15:38:26 -0500

    made our code more object oriented by putting related code within an object

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
+// adds new Task as an organized object
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logState: function() {
+      console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
+  return task;
+}
+
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
+task1.title;
+// for now, let's just make sure we see our tasks
+console.log(tasks);
+
+/*
+// prints out the provided tasks details
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+}
+
+// marks the provided task as completed
+function completeTask(task) {
+  task.complete = true;
+}
+*/
+
+
+/*
+
 // Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
@@ -29,3 +77,4 @@ newTask("Do Laundry"); // task 1
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
 logTaskState(0); // Clean Cat Litter has been completed
+*/
